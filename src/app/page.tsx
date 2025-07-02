a050702df4caaef4ba5e25a6f784f32b885bb28c
LEEVANCEE <1280403599@qq.com>
2025-07-02 14:23:04 +0800

    search params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
 import HomePage from "@/components/HomePage";
+import SearchResults from "@/components/SearchResults";
 
-export default function Home() {
-  return (
-   <HomePage/>
-  );
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+
+interface PageProps {
+  searchParams: SearchParams;
+}
+
+export default async function Home({ searchParams }: PageProps) {
+  const params = await searchParams;
+  const searchQuery = params.s as string;
+  console.log(params);
+
+  // 如果有搜索参数，显示搜索结果页面
+  if (searchQuery) {
+    return <SearchResults query={searchQuery} />;
+  }
+
+  // 否则显示首页
+  return <HomePage />;
 }
